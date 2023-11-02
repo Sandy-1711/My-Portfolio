30771c0b1aa7cc76172b7e16db7c415ebf3decde
Sandy-1711 <sandy1711003@gmail.com>
2023-11-02 18:56:09 +0530

    modified projectllist

--- src/components/projectllist.js
+++ src/components/projectllist.js
@@ -1,10 +1,10 @@
 const list=[
     {
         id:1,
-        title:"Hostel Management",
-        link:"https://g-five-hackfest-23.github.io/Hostel-Management/",
-        code:"https://github.com/G-Five-Hackfest-23/Hostel-Management",
-        desc:"This website was build during a hackathon conducted by IEEE Society of MMMUT, Gorakhpur. Use username- 2022041164 and password- 8953596712 on the student login page.",
+        title:"Web Accuracy",
+        link:"https://web-accuracy.netlify.app/",
+        code:"https://github.com/sandy-1711/wa",
+        desc:"Multi-page NextJS Website,Used technologies like gsap and lenin scroll to make beautiful frontend applications.",
     },
     {
         id:2,
@@ -13,34 +13,28 @@ const list=[
         code:"https://github.com/Sandy-1711/MyBook/",
         desc:"A full stack fully functional replica aka clone of Instagram built using MERN, Material UI, react-router-dom ",
     },
-    // {
-    //     id:2,
-    //     title:"QuadB React Project",
-    //     link:'https://sandy-1711.github.io/QuadB',
-    //     code:"https://github.com/Sandy-1711/QuadB/",
-    //     desc:"A React application that fetches data from API and visualise it on frontend. Built as an internship test project.",
-    // },
+    
     {
         id:3,
-        title:"DocSpot",
-        link:"https://sandy-1711.github.io/DTU-Hack/",
-        code:"https://github.com/Sandy-1711/DTU-Hack/",
-        desc:"This website was built during a hackathon conducted by Delhi Technical University. Backend and frontend could not be connected.",
+        title:"UIUX Landing Page",
+        link:"https://web-accuracy.netlify.app/services/ui-ux",
+        code:"https://github.com/sandy-1711/wa",
+        desc:"A landing page for a UIUX services website",
     },
     {
         id:4,
-        title:"MyBookAPI",
-        // link:"https://sandy-1711.github.io/DTU-Hack/",
-        code:"https://github.com/Sandy-1711/MyBookAPI/",
-        desc:"This is the backend code for MyBook Application. Made using NodeJS, ExpressJS, MongoDB, JWT ",
+        title:"AR VR MR service Landing Page",
+        link:"https://web-accuracy.netlify.app/3dtech/ar-vr-mr",
+        code:"https://github.com/sandy-1711/wa",
+        desc:"A landing page for a company providing ar vr mr service",
     },
     {
         id:5,
-        title:"innovat-a-thon",
-        link:"https://sandy-1711.github.io/innovat-a-thon/",
-        code:"https://github.com/Sandy-1711/innovat-a-thon",
-        desc:"This website was built under 1.5 hours during a contest conducted by Robotics Club of MMMUT.",
-    },
+        title:"Blockchain Service Landing Page",
+        link:"https://web-accuracy.netlify.app/trends/blockchain",
+        code:"https://github.com/sandy-1711/wa",
+        desc:"A landing page of a blockchain website",
+    }
     
 ];
 export default list;
